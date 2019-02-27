55cc053f85aacf5730f0eb19521ac9608dd0618e
zhengxiangjun <xiangjun>
2019-02-27 13:36:49 +0800

    基础代码 xiangjun

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -7,6 +7,8 @@ class DefaultController extends Controller
 {
     public function actionIndex()
     {
+        $response = \Yii::$app->response;
+        $response->format = \yii\web\Response::FORMAT_HTML;
         return $this->renderFile('@webroot/home/index.html');
     }
 
