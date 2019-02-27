287b198b55c9854bb2de2d5d0be39dcfa8f49ea3
jiayao chen <ayao17508959493@gmail.com>
2019-02-27 18:27:14 +0800

    检查登录逻辑更改    chenjiaguang

--- client/home/vue.config.js
+++ client/home/vue.config.js
@@ -1,12 +1,13 @@
 // vue.config.js
 module.exports = {
   // 选项...
-  publicPath: '/home/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/home/' : '/',
   outputDir: '../../web/home',
   assetsDir: 'assets',
   devServer: {
+    https: true,
     disableHostCheck: true,
-    proxy: 'http://arthometest.fantuan.cn'
+    proxy: 'https://arthometest.fantuan.cn'
   },
   chainWebpack: config => {
     config.plugins.delete('prefetch')
