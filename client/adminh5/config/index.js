97cce9bdf068ee50add380541e7a813a0985094e
杨青青 <502258389@qq.com>
2019-02-28 10:51:15 +0800

    xiugai

--- client/adminh5/config/index.js
+++ client/adminh5/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
         index: path.resolve(__dirname, '../../../web/admin/index.html'),
         assetsRoot: path.resolve(__dirname, '../../../web/admin'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: './',
         productionSourceMap: false,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
