3d7897837e315168025dbbc21eabcbd24236223b
zhengxiangjun <xiangjun>
2019-03-01 10:57:21 +0800

    基础代码 xiangjun

--- config/web.php
+++ config/web.php
@@ -21,6 +21,9 @@ $config = [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'session' => [
+            'name' => 'advanced',
+        ],
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
