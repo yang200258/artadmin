a4f42e4ae9926cf0a981456019b493417f1d2596
zhengxiangjun <xiangjun>
2019-03-08 13:55:30 +0800

    基础代码 xiangjun

--- controllers/ApplyController.php
+++ controllers/ApplyController.php
@@ -120,7 +120,7 @@ class ApplyController extends Controller
             return $this->error('请输入指导老师电话');
         }
         $transaction = \Yii::$app->db->beginTransaction();//创建事务
-//        try {
+        try {
             $apply = new Apply();
             $apply->apply_no = Apply::getApplynum($domain);
             $apply->uid = $this->user->id;
@@ -190,11 +190,11 @@ class ApplyController extends Controller
                 return $this->error('报名失败');
             }
             $transaction->commit();//提交事务
-//        } catch (\Exception $e) {
-//            \Yii::error($e->getMessage());
-//            $transaction->rollback();//回滚事务
-//            return $this->error('服务器繁忙，请稍后再试！');
-//        }
+        } catch (\Exception $e) {
+            \Yii::error($e->getMessage());
+            $transaction->rollback();//回滚事务
+            return $this->error('服务器繁忙，请稍后再试！');
+        }
         return $this->json(['id' => $apply->id]);
     }
 
